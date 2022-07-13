91b4887cd9a788a8d2798cada60f65e3dc832ed2
Pranav <pranavmagare2020@gmail.com>
2022-07-13 12:51:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Router from './Route';
+import WebCamera from './WebCamera';
+
+export default function App() {
+  return (
+ <div className="App">
+      <Router/>
+      {/* <WebCamera /> */}
+    </div>   
+  );
+} 
