99b6b05bbfb212a6d110c66816960d62c15c6fc0
Pranav Magare <vickymagare1996@gmail.com>
2022-07-26 15:54:08 +0530

    change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import Router from "./Route";
 import Content from "./Content";
-import Navbar from "./Components/SideMenu";
 import PopUpMenu from "./PopUpMenu";
 import Main from "./Main";
 import "./App.css";
@@ -11,9 +10,9 @@ export default function App() {
   return (
     <div className="App">
       <Main />
+      <Content />
       <Router />
       <PopUpMenu />
-      <Content />
     </div>
   );
 }
